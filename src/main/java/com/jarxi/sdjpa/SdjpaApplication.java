c24d86d034eeddf931f14ab61d0d12997d85e157
cjp <cjpthree@126.com>
2018-12-06 15:45:00 +0800

    delete unuse code

--- src/main/java/com/jarxi/sdjpa/SdjpaApplication.java
+++ src/main/java/com/jarxi/sdjpa/SdjpaApplication.java
@@ -2,16 +2,10 @@ package com.jarxi.sdjpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class SdjpaApplication extends SpringBootServletInitializer {
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(SdjpaApplication.class);
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(SdjpaApplication.class, args);
     }
