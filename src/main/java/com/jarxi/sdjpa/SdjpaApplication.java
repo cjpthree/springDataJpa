3f0d21fdde93c5c798b27fc11ee9590c902934a5
cjp <cjpthree@126.com>
2018-12-06 14:15:59 +0800

    init

---
+++ src/main/java/com/jarxi/sdjpa/SdjpaApplication.java
@@ -0,0 +1,12 @@
+package com.jarxi.sdjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdjpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdjpaApplication.class, args);
+    }
+}
